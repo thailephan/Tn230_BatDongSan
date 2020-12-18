f3fcf87dd7e6ec90af8ca6bab04d41910eed18c2
thailephan <thailephanminh@gmail.com>
2020-12-18 17:40:20 +0700

    Add Areas And Load the Database Fine

---
+++ ASPNET/BatDongSanMVC/TN230_BatDongSan/Areas/Admin/Controllers/KhuDanCusController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DbContextModel.Framework;
+
+namespace TN230_BatDongSan.Areas.Admin.Controllers
+{
+    public class KhuDanCusController : Controller
+    {
+        private DbContextWeb db = new DbContextWeb();
+
+        // GET: Admin/KhuDanCus
+        public ActionResult Index()
+        {
+            return View(db.KhuDanCus.ToList());
+        }
+
+        // GET: Admin/KhuDanCus/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            KhuDanCu khuDanCu = db.KhuDanCus.Find(id);
+            if (khuDanCu == null)
+            {
+                return HttpNotFound();
+            }
+            return View(khuDanCu);
+        }
+
+        // GET: Admin/KhuDanCus/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/KhuDanCus/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "MaKhuDanCu,TenKhuDanCu")] KhuDanCu khuDanCu)
+        {
+            if (ModelState.IsValid)
+            {
+                db.KhuDanCus.Add(khuDanCu);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(khuDanCu);
+        }
+
+        // GET: Admin/KhuDanCus/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            KhuDanCu khuDanCu = db.KhuDanCus.Find(id);
+            if (khuDanCu == null)
+            {
+                return HttpNotFound();
+            }
+            return View(khuDanCu);
+        }
+
+        // POST: Admin/KhuDanCus/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "MaKhuDanCu,TenKhuDanCu")] KhuDanCu khuDanCu)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(khuDanCu).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(khuDanCu);
+        }
+
+        // GET: Admin/KhuDanCus/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            KhuDanCu khuDanCu = db.KhuDanCus.Find(id);
+            if (khuDanCu == null)
+            {
+                return HttpNotFound();
+            }
+            return View(khuDanCu);
+        }
+
+        // POST: Admin/KhuDanCus/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            KhuDanCu khuDanCu = db.KhuDanCus.Find(id);
+            db.KhuDanCus.Remove(khuDanCu);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ASPNET/BatDongSanMVC/TN230_BatDongSan/Areas/Admin/Controllers/QuanHuyensController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DbContextModel.Framework;
+
+namespace TN230_BatDongSan.Areas.Admin.Controllers
+{
+    public class QuanHuyensController : Controller
+    {
+        private DbContextWeb db = new DbContextWeb();
+
+        // GET: Admin/QuanHuyens
+        public ActionResult Index()
+        {
+            return View(db.QuanHuyens.ToList());
+        }
+
+        // GET: Admin/QuanHuyens/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            QuanHuyen quanHuyen = db.QuanHuyens.Find(id);
+            if (quanHuyen == null)
+            {
+                return HttpNotFound();
+            }
+            return View(quanHuyen);
+        }
+
+        // GET: Admin/QuanHuyens/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/QuanHuyens/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "MaQuanHuyen,TenQuanHuyen")] QuanHuyen quanHuyen)
+        {
+            if (ModelState.IsValid)
+            {
+                db.QuanHuyens.Add(quanHuyen);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(quanHuyen);
+        }
+
+        // GET: Admin/QuanHuyens/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            QuanHuyen quanHuyen = db.QuanHuyens.Find(id);
+            if (quanHuyen == null)
+            {
+                return HttpNotFound();
+            }
+            return View(quanHuyen);
+        }
+
+        // POST: Admin/QuanHuyens/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "MaQuanHuyen,TenQuanHuyen")] QuanHuyen quanHuyen)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(quanHuyen).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(quanHuyen);
+        }
+
+        // GET: Admin/QuanHuyens/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            QuanHuyen quanHuyen = db.QuanHuyens.Find(id);
+            if (quanHuyen == null)
+            {
+                return HttpNotFound();
+            }
+            return View(quanHuyen);
+        }
+
+        // POST: Admin/QuanHuyens/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            QuanHuyen quanHuyen = db.QuanHuyens.Find(id);
+            db.QuanHuyens.Remove(quanHuyen);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
