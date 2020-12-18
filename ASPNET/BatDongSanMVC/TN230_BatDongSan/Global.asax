25a66634418104cbb157827627b4a288292c3a91
thailephan <thailephanminh@gmail.com>
2020-12-18 14:36:04 +0700

    first commit

---
+++ ASPNET/BatDongSanMVC/TN230_BatDongSan/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TN230_BatDongSan.MvcApplication" Language="C#" %>
---
+++ ASPNET/BatDongSanMVC/TN230_BatDongSan/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TN230_BatDongSan.Startup))]
+namespace TN230_BatDongSan
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
